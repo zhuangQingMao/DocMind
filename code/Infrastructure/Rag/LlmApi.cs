def34ab8ddcb33783d8d13605b6d542e8b28bef3
zhuangQingMao <1726580476@qq.com>
2025-12-29 21:24:26 +0800

    infrastructure改造。

---
+++ code/Infrastructure/Rag/LlmApi.cs
@@ -0,0 +1,8 @@
+﻿namespace DocMind
+{
+    public class LlmApi
+    {
+        public static string ApiKey = "sk-Tkp8vuihQTOJ6GUFjbFIfUfBhhxq385zq69ZfWzsXxByHGkl";
+        public static string ApiEndpoint = "https://api.probex.top/v1/chat/completions";
+    }
+}
