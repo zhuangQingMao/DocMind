3399eed6d097ba137a0ec93af7f332bb5d3a77c7
zhuangQingMao <1726580476@qq.com>
2026-01-17 09:42:54 +0800

    app.xaml.cs重构

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,46 +4,35 @@ using System.Windows;
 
 namespace DocMind
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        // 定义 DI 容器
-        public IServiceProvider ServiceProvider { get; private set; }
+        public required IServiceProvider ServiceProvider { get; set; }
 
         protected override async void OnStartup(StartupEventArgs e)
         {
             var serviceCollection = new ServiceCollection();
+
             await ConfigureServices(serviceCollection);
 
             ServiceProvider = serviceCollection.BuildServiceProvider();
 
             var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+
             mainWindow.Show();
         }
 
-        private async Task ConfigureServices(IServiceCollection services)
+        private static async Task ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<MainWindow>();
             services.AddSingleton<IMessenger>(WeakReferenceMessenger.Default);
             services.AddSingleton<ChatViewModel>();
-
             services.AddSingleton<IRagCoreService, RagCoreService>();
             services.AddSingleton<IEmbeddingService, EmbeddingService>();
-            var repository = await VectorRepository.CreateAsync();
-            services.AddSingleton<IVectorRepository>(repository);
-
+            services.AddSingleton<IVectorRepository>(await VectorRepository.CreateAsync());
             services.AddSingleton<IFileManagerFactory, FileManagerFactory>();
             services.AddSingleton<IDocumentLoaderService, DocumentLoaderService>();
-
             services.AddSingleton<IDocumentChunker, DocumentChunker>();
-
             services.AddSingleton<ILLMChatService, LLMChatService>();
-
-
-
-
         }
     }
 }
